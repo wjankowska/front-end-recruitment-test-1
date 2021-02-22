ce46f2eb7165c799091a8bf3dd76297491c34a14
Weronika Jankowska <weronika.jankowska098@gmail.com>
2021-02-22 23:02:04 +0100

    Task 2

--- app/scripts/Bacon/BaconManager.js
+++ app/scripts/Bacon/BaconManager.js
@@ -4,14 +4,12 @@ export default class BaconManager {
     constructor(baconImageSrc, container) {
         this.baconImageSrc = baconImageSrc;
         this.container = container;
-
-        this.baconImages = [];
     }
 
     addBacon() {
         const baconImage = new BaconImage(this.baconImageSrc);
         const element = baconImage.createElement();
-        
+
         this.container.appendChild(element);
     }
 }
